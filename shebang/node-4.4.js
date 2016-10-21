46ba356a632ff4e783d684096c4ae16bad96d612
swxy <swxymile@163.com>
2016-10-21 17:34:53 +0800

    add shebang test script

---
+++ shebang/node-4.4.js
@@ -0,0 +1,3 @@
+#!/Users/baidu/.nvm/versions/node/v4.4.5/bin/node
+
+console.log('当前node的版本是：' + process.version);
---
+++ shebang/node-6.9.js
@@ -0,0 +1,3 @@
+#!/Users/baidu/.nvm/versions/node/v6.9.1/bin/node
+
+console.log('当前node的版本是：' + process.version);
