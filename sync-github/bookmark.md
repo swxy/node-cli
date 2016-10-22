b4adeaf261c40004fcee0134f0e8f542532fdb5f
swxy <swxymile@163.com>
2016-10-22 16:10:48 +0800

    sync to github

---
+++ sync-github/bookmark.md
@@ -0,0 +1,4 @@
+### 2016-10-22<br />
++ [ECMAScript 2015 Language Specification – ECMA-262 6th Edition](http://www.ecma-international.org/ecma-262/6.0/)<br />
++ [25. Promises for asynchronous programming](http://exploringjs.com/es6/ch_promises.html)<br />
++ [javascript](http://agroup.baidu.com/fe-styleguide/md/article/62439#2-%E4%BB%A3%E7%A0%81%E9%A3%8E%E6%A0%BC)<br />
---
+++ sync-github/server.js
@@ -0,0 +1,95 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const readline = require('readline');
+const querystring = require('querystring');
+const spawnSync = require('child_process').spawnSync;
+
+http.createServer((req, res) => {
+    let urlData = url.parse(req.url);
+    let queryObj = querystring.parse(urlData.query);
+    if (queryObj.title && queryObj.url) {
+        addBookmark(queryObj).then(() => {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                code: 0,
+                msg : 'success'
+            }));
+        }).catch((err) => {
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({
+                code: 500,
+                msg : err
+            }));
+        });
+    }
+    else {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({
+            code: 0,
+            msg : 'title & url required'
+        }));
+    }
+
+}).listen(8180);
+
+function addBookmark(obj) {
+    let fileName = 'bookmark.md';
+    let data = [];
+    let inserted = false;
+    const date = new Date();
+
+    const rl = readline.createInterface({
+        input: fs.createReadStream(fileName)
+    });
+
+    return new Promise((resolve, reject) => {
+        rl.on('line', (line) => {
+            if (!inserted && line.startsWith('###')) {
+
+                let date_str = line.match(/\d{4}-\d{1,2}-\d{1,2}/)[0];
+                if (date_str && (new Date(date_str).getDate() === date.getDate())) {
+                    data.push(line);
+                    data.push(`+ [${obj.title}](${obj.url})<br />`);
+                }
+                else {
+                    data.push(`### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}<br />`)
+                    data.push(`+ [${obj.title}](${obj.url})<br />\n`);
+                    data.push(line);
+                }
+                inserted = true;
+            }
+            else {
+                data.push(line);
+            }
+        });
+        rl.on('close', () => {
+            if (!inserted) { //新建的文件
+                data.push(`### ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}<br />`)
+                data.push(`+ [${obj.title}](${obj.url})<br />\n`);
+            }
+            fs.writeFile(fileName, data.join('\n'), (err) => {
+                if (err){
+                    console.error(err);
+                    reject(err);
+                }
+                console.log('saved ' + fileName);
+                resolve();
+                //syncToGithub(title);
+            });
+        });
+    });
+}
+
+function syncToGithub(title) {
+    var cmds = [
+        ['git', ['add', '.']],
+        ['git', ['commit', '-am', `"add ${title || ''}"`]],
+        ['git', ['pull', '--rebase']],
+        ['git', ['push', 'origin', 'master']]
+    ];
+    cmds.forEach((cmd) => {
+        let result = spawnSync(cmd[0], cmd[1]);
+        console.log(result.output.join('\n'));
+    });
+}
