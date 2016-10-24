d4d7f0904334de06280a1143e9ae0235cc908685
swxy <swxymile@163.com>
2016-10-24 20:28:04 +0800

    add global

---
+++ global/hello.js
@@ -0,0 +1,15 @@
+#!/Users/baidu/.nvm/versions/node/v6.9.1/bin/node
+
+console.log('当前node的版本是：' + process.version);
+
+// -rw-r--r--  ==> -rwxr-xr-x
+`
+
+/Users/baidu/.nvm/versions/node/v6.9.1/bin/hello  -> 
+/Users/baidu/.nvm/versions/node/v6.9.1/lib/node_modules/global/hello.js
+
+/Users/baidu/.nvm/versions/node/v6.9.1/lib/node_modules/global -> 
+/Users/baidu/workplace/git/node-cli/global
+
+`
+// /Users/baidu/.nvm/versions/node/v6.9.1/lib/node_modules/global -> /Users/baidu/workplace/git/node-cli/global
